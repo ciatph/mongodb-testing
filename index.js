1dbcc32efbea6abee50dd61f42f8cf21468e6f28
ciatph <ciat.ph@gmail.com>
2019-10-10 17:23:57 +0800

    Used mongoose to connect to mongodb.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// Connect to mongoDB
+mongoose.connect(`mongodb://localhost/test`, {useNewUrlParser: true, useUnifiedTopology: true})
+const mongodb = mongoose.connection
+
+mongodb.on('error', console.error.bind(console, 'connection error:'))
+mongodb.on('open', function() {
+  console.log('connected!')
+})
+
+// App
 app.get('/', (req, res, next) => {
   res.send('okay')
 })
